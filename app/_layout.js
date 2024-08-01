d826b8c1b102e8654d7812d79b5b35632879ae91
Lakshan <perera.alc2000@gmail.com>
2024-08-01 10:57:30 +0530

    font removed

--- app/_layout.js
+++ app/_layout.js
@@ -1,29 +1,7 @@
-import { Stack } from "expo-router";
-import { useCallback } from "react";
-import { useFonts } from "expo-font";
-import * as SplashScreen from "expo-splash-screen";
+import {Stack} from "expo-router";
 
-SplashScreen.preventAutoHideAsync()
-const Layout = () =>
-{
-    const [fontsLoaded] = useFonts({
-        DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
-        DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
-        DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
-    })
-
-    const onLayoutRootView = useCallback(async () =>
-    {
-        if (fontsLoaded)
-        {
-            await SplashScreen.hideAsync()
-        }
-    }, [fontsLoaded])
-
-    if (!fontsLoaded) return null;
-
-    return <Stack onLayout={onLayoutRootView} />;
-
+const Layout = () => {
+    return <Stack/>;
 }
 
 export default Layout;
--- components/home/popular/popularjobs.style.js
+++ components/home/popular/popularjobs.style.js
@@ -1,29 +1,19 @@
-import { StyleSheet } from "react-native";
+import {StyleSheet} from "react-native";
 
-import { FONT, SIZES, COLORS } from "../../../constants";
+import {COLORS, SIZES} from "../../../constants";
 
 const styles = StyleSheet.create({
-  container: {
-    marginTop: SIZES.xLarge,
-  },
-  header: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  headerTitle: {
-    fontSize: SIZES.large,
-    fontFamily: FONT.medium,
-    color: COLORS.primary,
-  },
-  headerBtn: {
-    fontSize: SIZES.medium,
-    fontFamily: FONT.medium,
-    color: COLORS.gray,
-  },
-  cardsContainer: {
-    marginTop: SIZES.medium,
-  },
+    container: {
+        marginTop: SIZES.xLarge,
+    }, header: {
+        flexDirection: "row", justifyContent: "space-between", alignItems: "center",
+    }, headerTitle: {
+        fontSize: SIZES.large, color: COLORS.primary,
+    }, headerBtn: {
+        fontSize: SIZES.medium, color: COLORS.gray,
+    }, cardsContainer: {
+        marginTop: SIZES.medium,
+    },
 });
 
 export default styles;
